3e19a87864eefbb6992f330072effe61c35273c8
Anderson <andersonfcanel@gmail.com>
2019-10-19 17:16:41 -0300

    get and post via at

--- SAA/SAA.ino
+++ SAA/SAA.ino
@@ -1,5 +1,7 @@
-/**/
+
+//Carrega as bibliotecas ESP8266 e SoftwareSerial"
 #include <SoftwareSerial.h>
+#include "ESP8266.h"
 
 /*VARIAVEIS DOS LEDS*/
 #define ledVerde 13
@@ -31,154 +33,166 @@ int tipoPinos[10] = {OUTPUT, OUTPUT, OUTPUT, OUTPUT, INPUT, INPUT, INPUT, INPUT,
 
 
 /*CONFIGURAÇÃO WIFI*/
-SoftwareSerial esp8266 (2, 3); //Arduino TX (ESP8266 RX) connected to Arduino Pin 2, Arduino RX(ESP8266 TX) connected to Arduino Pin 3
+SoftwareSerial esp8266(2, 3); //Arduino TX (ESP8266 RX) connected to Arduino Pin 2, Arduino RX(ESP8266 TX) connected to Arduino Pin 3
 
-String ssid = "ALHN-8E28";
-String password = "4650730851";
+String ssid = "Romero";
+String password = "35475442";
 
-String path = "/api/arduino/oi";
-String server = "192.168.1.70";
-String request = "GET " + path + " HTTP/1.1\r\n" + "Host: " + server + "\r\n" + "Connection: keep-alive\r\n\r\n";
-String requestLength = String(request.length());
+//String ssid = "TheHell";
+//String password = "ahp15161522";
 
-const int timeout = 2000;
+//Ip do WebServer
+String server = "192.168.0.16";
+
+//Porta do Servidor
+String port  = "8090";
+
+const int timeout = 3000;
 
 void setup()
 {
   // Inicializa o Monitor Serial porta 9600
-    Serial.begin(9600);
+  Serial.begin(9600);
 
   //inicializa todos os pinModes
   for (int i = 0; i < sizeof(pinosUsados); i++) {
     pinMode(pinosUsados[i], tipoPinos[i]);
   }
+
   desligaTudo();
   Nivel_Minimo_Para_Acionamento = NivelDeAcionamentoMinimo(1);
 
-  //esp8266.begin(9600); // Should match ESP's current baudrate
- 
+
+  //ESP8266 CONFIG
+  esp8266.begin(9600); // Should match ESP's current baudrate
+  Serial.println(getMacAdress());
   //setupESP8266();
   //connectToWiFi();
-  //startTCPConnection();  
-  //sendGetRequest();
-  //closeTCPConnection();
-  
+  wifisetup();  //wifi setup
+  connectWifi();
+  //Abrindo conexão TCP
+  startTCPConnection();
+  sendPostRequest( "{\"logado\":\"logado\"}", "/api/arduino/deviceConect" );
+  closeTCPConnection();
+
 }
 
+
 void loop() {
-
+  if (0) {
     //Toda a lógica de funcionamento deve ocorrer no interior desse if
-  //Se circuito CS FECHADO então...(ÁGUA NO CANO)
+    //Se circuito CS FECHADO então...(ÁGUA NO CANO)
 
-  boolean  contSeco = temAgua();
-  boolean n0 = checaN0();
-  boolean n1 = checaN1();
-  boolean n2 = checaN2();
-  boolean n3 = checaN3();
+    boolean  contSeco = temAgua();
+    boolean n0 = checaN0();
+    boolean n1 = checaN1();
+    boolean n2 = checaN2();
+    boolean n3 = checaN3();
 
-  //CHECAR NÍVEL 0
-  if (contSeco ) {
-    // se o circuito de nivel0 estiver FECHADO (BOIA 0 LEVANTADA)
-    if (n0 ) {
-      Serial.println( "NIVEL-0");
-      rotinaN0Levantado();
-      defineNivel_Atual(0);
-      //CHECAR NÍVEL 1
-      if (n1 && n0 ) {
-        Serial.println( "NIVEL-1");
-        rotinaN1Levantado();
-        defineNivel_Atual(1);
-        //CHECAR NÍVEL 2
-        if (n2 && n1 && n0 ) {
-          Serial.println( "NIVEL-N2");
-          rotinaN2Levantado();
-          defineNivel_Atual(2);
-          //CHECAR NÍVEL 3
-          if (n3 && n2 && n1 && n0 ) {
-            Serial.println( "NIVEL-N3");
-            rotinaN3Levantado();
-            defineNivel_Atual(3);
-            desligaBomba();
+    //CHECAR NÍVEL 0
+    if (contSeco ) {
+      // se o circuito de nivel0 estiver FECHADO (BOIA 0 LEVANTADA)
+      if (n0 ) {
+        Serial.println( "NIVEL-0");
+        rotinaN0Levantado();
+        defineNivel_Atual(0);
+        //CHECAR NÍVEL 1
+        if (n1 && n0 ) {
+          Serial.println( "NIVEL-1");
+          rotinaN1Levantado();
+          defineNivel_Atual(1);
+          //CHECAR NÍVEL 2
+          if (n2 && n1 && n0 ) {
+            Serial.println( "NIVEL-N2");
+            rotinaN2Levantado();
+            defineNivel_Atual(2);
+            //CHECAR NÍVEL 3
+            if (n3 && n2 && n1 && n0 ) {
+              Serial.println( "NIVEL-N3");
+              rotinaN3Levantado();
+              defineNivel_Atual(3);
+              desligaBomba();
+            } else {
+              rotinaN3Abaixado();
+              ligaBomba();
+              Serial.println( "falseN3");
+            }
+
+            //desligaBomba();
           } else {
-            rotinaN3Abaixado();
+            rotinaN2Abaixado();
             ligaBomba();
-            Serial.println( "falseN3");
+            Serial.println( "falseN2");
           }
 
           //desligaBomba();
         } else {
-          rotinaN2Abaixado();
+          rotinaN1Abaixado();
           ligaBomba();
-          Serial.println( "falseN2");
+          Serial.println( "falseN1");
         }
-
         //desligaBomba();
       } else {
-        rotinaN1Abaixado();
+        Serial.println( "falseN0");
+        rotinaN0Abaixado();
         ligaBomba();
-        Serial.println( "falseN1");
       }
-      //desligaBomba();
-    } else {
-      Serial.println( "falseN0");
-      rotinaN0Abaixado();
-      ligaBomba();
     }
-  }
 
-  //Se circuito CS ABERTO então...(SECO NO CANO)
-  else {
-    // se o circuito de nivel0 estiver FECHADO (BOIA 0 LEVANTADA)
-    if (n0) {
-      Serial.println( "trueN0");
-      rotinaN0Levantado();
+    //Se circuito CS ABERTO então...(SECO NO CANO)
+    else {
+      // se o circuito de nivel0 estiver FECHADO (BOIA 0 LEVANTADA)
+      if (n0) {
+        Serial.println( "trueN0");
+        rotinaN0Levantado();
 
-      //CHECAR NÍVEL 1
-      if (n1) {
-        Serial.println( "trueN1");
-        rotinaN1Levantado();
-        //CHECAR NÍVEL 2
-        if (n2) {
-          Serial.println( "trueN2");
-          rotinaN2Levantado();
-          //CHECAR NÍVEL 3
-          if (n3) {
-            Serial.println( "trueN3");
-            rotinaN3Levantado();
-            desligaBomba();
+        //CHECAR NÍVEL 1
+        if (n1) {
+          Serial.println( "trueN1");
+          rotinaN1Levantado();
+          //CHECAR NÍVEL 2
+          if (n2) {
+            Serial.println( "trueN2");
+            rotinaN2Levantado();
+            //CHECAR NÍVEL 3
+            if (n3) {
+              Serial.println( "trueN3");
+              rotinaN3Levantado();
+              desligaBomba();
+            } else {
+              rotinaN3Abaixado();
+              desligaBomba();
+            }
+
+            //desligaBomba();
           } else {
-            rotinaN3Abaixado();
+            rotinaN2Abaixado();
             desligaBomba();
           }
 
           //desligaBomba();
         } else {
-          rotinaN2Abaixado();
+          rotinaN1Abaixado();
           desligaBomba();
         }
-
         //desligaBomba();
       } else {
-        rotinaN1Abaixado();
+        Serial.println( "falseN0");
+        rotinaN0Abaixado();
         desligaBomba();
       }
-      //desligaBomba();
-    } else {
-      Serial.println( "falseN0");
-      rotinaN0Abaixado();
+
+      //configurar um delay de 5 minutos
+      delay(4000);
       desligaBomba();
     }
 
-    //configurar um delay de 5 minutos
-    delay(4000);
-    desligaBomba();
+    // espera 1 segundo para o proximo loop
+    delay(1000);
+    Serial.println();
+    Serial.println();
+
   }
-
-  // espera 1 segundo para o proximo loop
-  delay(1000);
-  Serial.println();
-  Serial.println();
-
 }
 
 //Checa contra seco
@@ -225,6 +239,7 @@ void desligaBomba () {
   //Avisa WS que DESLIGOU bomba
   //***********************************
 }
+
 
 void desligaTudo() {
   //liga o led vermelho
@@ -425,14 +440,16 @@ int defineNivel_Atual( int nivel) {
 
 
 /*WIFI CONFIG*/
+
+//Metodo para enviar comandos AT para ESP8266
 String atCommand(String command, int timeout) {
   String response = "";
   esp8266.println(command);
 
   long int time = millis();
 
-  while( (time+timeout) > millis() ) {
-    while(esp8266.available()) {
+  while ( (time + timeout) > millis() ) {
+    while (esp8266.available()) {
       char c = esp8266.read();
       response += c;
     }
@@ -442,35 +459,123 @@ String atCommand(String command, int timeout) {
   return response;
 }
 
-void setupESP8266() {
-  atCommand("AT+RST", timeout);
-  atCommand("AT+CWMODE=1", timeout);
+
+
+//Médodo para enviar rquisisão Post
+String sendPostRequest( String msg, String uri ) {
+  //Post- EXEMPLO
+  //String pathPost = "/api/arduino/oiPost"; //MODELO URI
+  //String msg = "{\"o\":\"o\"}";//MODELO MSG
+
+  String requestPost =     "POST " + uri + " HTTP/1.1\r\n"
+                           + "Host: " + server + ":" + port + "\r\n"
+                           + "Accept: *" + "/" + "*\r\n"
+                           + "Content-Length: " + msg.length() + "\r\n"
+                           + "Connection: keep-alive\r\n"
+                           + "Content-Type: application/json\r\n\r\n"
+                           + msg + "\r\n";
+
+  String requestLengthPost = String(requestPost.length());
+  atCommand("AT+CIPSEND=" + requestLengthPost, timeout);
+  String response = atCommand(requestPost, 6000);
+  //Serial.println(response);
+  return response;
 }
 
-void connectToWiFi() {
-  String connect = "AT+CWJAP=\"" +ssid+"\",\"" + password + "\"";
-  atCommand(connect, 6000);
-  atCommand("AT+CIFSR", timeout);
+
+//Médodo para enviar rquisisão get
+String sendGetRequest(  String uri )
+{
+  //GET - EXEMPLO
+  //String path = "/api/arduino/oi?oi=123";
+  //String path = "/api/arduino/oi";
+
+  String requestGet = "GET " + uri + " HTTP/1.1\r\n"
+                      + "Host: " + server + "\r\n"
+                      + "Connection: keep-alive\r\n\r\n";
+
+  String requestLengthGet = String(requestGet.length());
+
+  atCommand("AT+CIPSEND=" + requestLengthGet, timeout);
+  String response = atCommand(requestGet, 6000);
+  //Serial.println(response);
+  return response;
 }
 
+
+//Metodo para Conexão wifi - Com Tentativas recursivas
+void connectWifi()//wificonnection
+{
+
+  String cmd = "AT+CWJAP=\"" + ssid + "\",\"" + password + "\"";
+  esp8266.println(cmd);
+  delay(3000);
+
+  if (esp8266.find("OK")) {
+    Serial.println("WIFI Connected!");
+    delay(1000);
+  }
+  else {
+
+    Serial.println("Cannot connect to wifi");
+    connectWifi();
+  }
+}
+
+//Metodo para resetar o esp8266
+void reset() {
+  esp8266.println("AT+RST");
+  delay(1000);
+  if (esp8266.find("OK") ) Serial.println("Module Reset");
+}
+
+//Metodo para configurar esp8266  e velocidade
+void esp_buad_rate_change()
+{
+  esp8266.println("AT+CIOBAUD=9600");                  //change buad rate
+  Serial.println("AT+CIOBAUD=9600");
+}
+
+
+//Metodo para configurar wifi
+void wifisetup()
+{
+  esp8266.println("AT");
+  esp8266.println("AT+CWMODE=3");//Comando AT :  3: SoftAP+Station mode
+  Serial.println("Wifi ready");
+}
+
+
+
+//Metodo para abrir TCP
 void startTCPConnection() {
   String connect = "AT+CIPSTART=\"TCP\",\"" + server + "\",8090";
   atCommand(connect, timeout);
 }
 
+
+//Metodo para fchar TCP
 void closeTCPConnection() {
   atCommand("AT+CIPCLOSE", timeout);
 }
 
-String sendGetRequest() {
-  atCommand("AT+CIPSEND=" + requestLength, timeout);
-  String response = atCommand(request, 6000);
-  return response;
+
+//Metodo para fchar TCP
+String getMacAdress() {
+  atCommand("AT+CIPAPMAC?", timeout);
 }
 
 
+/*void setupESP8266() {
+  atCommand("AT+RST", timeout);
+  atCommand("AT+CWMODE=1", timeout);
+  }
 
-
+  void connectToWiFi() {
+  String connect = "AT+CWJAP=\"" +ssid+"\",\"" + password + "\"";
+  atCommand(connect, 6000);
+  atCommand("AT+CIFSR", timeout);
+  }*/
 
 /*
   void cheio(boolean cheio) {
